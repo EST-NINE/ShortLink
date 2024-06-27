75b3fa3d6606723a0cae76f65f39247858d25b6b
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-06-27 21:06:19 +0800

    feat: 新增更改用户信息功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.admin.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -21,6 +22,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 判断用户名是否存在
+     *
      * @param username 用户名
      * @return 用户名存在返回 true:存在，false:不存在
      */
@@ -29,7 +31,15 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 注册用户
+     *
      * @param requestParam 注册用户请求参数
      */
     void register(UserRegisterReqDTO requestParam);
+
+    /**
+     * 更新用户信息
+     *
+     * @param requestParam 更新用户信息请求参数
+     */
+    void update(UserUpdateReqDTO requestParam);
 }
