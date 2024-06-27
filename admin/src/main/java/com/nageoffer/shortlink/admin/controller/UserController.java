149843669dd4c7d8dad592291cd6b4d82c5c7014
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-06-27 11:32:22 +0800

    feat: 添加查询用户信息功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     /**
      * 根据用户名获取用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public String getUserByUserName(@PathVariable("username") String username) {
-        return "Hello, " + username;
+    public UserRespDTO getUserByUserName(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
     }
 }
