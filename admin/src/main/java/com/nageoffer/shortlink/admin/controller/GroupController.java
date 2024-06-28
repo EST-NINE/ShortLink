6af45d06b4c92a594fb2dd27de035270ce9eae7f
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-06-28 15:24:03 +0800

    refactor: 重构短链接后管系统基础路径

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -24,7 +24,7 @@ public class GroupController {
     /**
      * 创建短链接分组
      */
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
@@ -33,7 +33,7 @@ public class GroupController {
     /**
      * 查询短链接分组列表
      */
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
     }
@@ -41,7 +41,7 @@ public class GroupController {
     /**
      * 修改短链接分组名称
      */
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/api/short-link/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam) {
         groupService.update(requestParam);
         return Results.success();
@@ -50,7 +50,7 @@ public class GroupController {
     /**
      * 删除短链接分组名称
      */
-    @DeleteMapping("/api/short-link/v1/group")
+    @DeleteMapping("/api/short-link/admin/v1/group")
     public Result<Void> deleteGroup(@RequestParam String gid) {
         groupService.deleteGroup(gid);
         return Results.success();
@@ -59,7 +59,7 @@ public class GroupController {
     /**
      * 排序短链接分组
      */
-    @PostMapping("/api/short-link/v1/group/sort")
+    @PostMapping("/api/short-link/admin/v1/group/sort")
     public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortDTO> requestParam) {
         groupService.sortGroup(requestParam);
         return Results.success();
