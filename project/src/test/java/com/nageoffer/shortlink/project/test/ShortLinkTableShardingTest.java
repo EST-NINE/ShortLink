cb88c43b31570b8a9847860dac671299d6663778
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-06-30 09:52:09 +0800

    feat: 添加路由表，通过短连接的 shortUri 来获取到短连接的 gid，从而得到短连接的 originalUri

--- project/src/test/java/com/nageoffer/shortlink/project/test/ShortLinkTableShardingTest.java
+++ project/src/test/java/com/nageoffer/shortlink/project/test/ShortLinkTableShardingTest.java
@@ -2,7 +2,8 @@ package com.nageoffer.shortlink.project.test;
 
 public class ShortLinkTableShardingTest {
 
-    public static String SQL = "CREATE TABLE `t_link_%d` (\n" +
+    // 短连接表
+    public static String SQL1 = "CREATE TABLE `t_link_%d` (\n" +
             "  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
             "  `domain` VARCHAR(128) DEFAULT NULL COMMENT '域名',\n" +
             "  `short_uri` VARCHAR(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '短链接',\n" +
@@ -22,6 +23,7 @@ public class ShortLinkTableShardingTest {
             "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
             ") ENGINE=INNODB AUTO_INCREMENT=1762385842304753667 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
 
+    // 短连接分组表
     public static String SQL2 = "create table `t_group_%d`\n" +
             "(\n" +
             "    `id`          bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
@@ -36,9 +38,18 @@ public class ShortLinkTableShardingTest {
             "    UNIQUE KEY `idx_unique_username_gid` (gid, username) USING BTREE\n" +
             ")ENGINE=InnoDB AUTO_INCREMENT=1716734146606301186 DEFAULT CHARSET=utf8mb4;";
 
+    // 路由表，通过短连接的 shortUri 来获取到短连接的 gid
+    // 原因是短连接是通过 gid 进行分片的，但我们的跳转需要通过 shortUri 来获取到 fullUri 的，所以需要一张路由表
+    public static String SQL3 = "CREATE TABLE `t_link_goto_%d`(\n" +
+            "      `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "      `gid` varchar(32) DEFAULT 'default' COMMENT  '分组标识',\n" +
+            "      `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "      PRIMARY KEY (`id`))\n" +
+            "      ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
-            System.out.printf((SQL2) + "%n",i);
+            System.out.printf((SQL3) + "%n",i);
         }
     }
 }
